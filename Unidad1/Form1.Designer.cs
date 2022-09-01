dde8363fd6245ff07bba966eff0373b30931dc02
juand <juand@JUANDA>
2022-09-01 16:31:00 -0500

    Agregar archivos de proyecto.

---
+++ Unidad1/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Unidad1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(185)))), ((int)(((byte)(55)))));
+            this.button1.Location = new System.Drawing.Point(48, 157);
+            this.button1.Name = "button1";
+            this.button1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.button1.Size = new System.Drawing.Size(171, 59);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Guardar";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(106, 51);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(175, 22);
+            this.txtNombre.TabIndex = 1;
+            this.txtNombre.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(24, 51);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(56, 16);
+            this.lblNombre.TabIndex = 2;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(185)))), ((int)(((byte)(55)))));
+            this.button2.Location = new System.Drawing.Point(251, 157);
+            this.button2.Name = "button2";
+            this.button2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.button2.Size = new System.Drawing.Size(171, 59);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Array";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(185)))), ((int)(((byte)(55)))));
+            this.button3.Location = new System.Drawing.Point(447, 157);
+            this.button3.Name = "button3";
+            this.button3.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.button3.Size = new System.Drawing.Size(171, 59);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "List";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(185)))), ((int)(((byte)(55)))));
+            this.button4.Location = new System.Drawing.Point(640, 157);
+            this.button4.Name = "button4";
+            this.button4.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.button4.Size = new System.Drawing.Size(171, 59);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "EXCEP";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(835, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Unidad1/Form1.cs
@@ -0,0 +1,331 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Unidad1
+{
+    public partial class Form1 : Form
+    {
+        int Numero = 0;
+       
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            int Num1 = 0;
+            long Num2;
+            char ch;
+            Double db;
+            string nombre = txtNombre.Text;
+            object casa = new { techo = "", puerta = "" };
+            var cosa = Numero;
+
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int Num1 , Num2 = 0;
+            long Num3;
+            char ch;
+            Double db;
+            string nombre = txtNombre.Text;
+            string substr;
+
+            // CONVERS
+            //cast X class
+            //db = Double.Parse(txtNombre.Text);
+            //Num1 = Int32.Parse(txtNombre.Text);
+            //bool conv = Int32.TryParse(txtNombre.Text, out Num1)
+
+            //Num1 = nombre.Length;
+            //nombre = nombre.Trim();
+            //substr = nombre.Substring(0, 5);
+            //var temp = nombre.Replace("J", "D");
+
+            // Asig Compuesta
+            //Num1++;
+            //Num1--;
+            //Num1 += 40;
+
+            //Num2 = suma(10 , 20 , 50);
+            //var resul = suma("2", "4");
+
+            ///// Concat and Format
+            string resultado = "";
+            ////resultado += " Juan";
+            //resultado = string.Format("Mi Nombre es:{0} y la suma es: {1} Final",nombre,Num2.ToString());
+            ////MessageBox.Show(String.Concat("El Resultado es:", nombre, " - ", temp, " Final"));
+
+            // Condicional
+            if(txtNombre.Text.Equals(""))
+            {
+                resultado = "";
+            }
+            else if (nombre == "")
+            {
+
+            }
+            else { }
+
+
+            //  Switch
+
+            switch (nombre)
+            {
+
+                case "Juan":
+                    Num1 = 1;
+                    break;
+                case "David":
+                    Num1 = 1;
+                    break;
+
+                default:
+                    break;
+            }
+
+
+
+            /// Ciclos 
+            /// 
+           
+            Num2 = 0;
+            while(Num2  <= 10)
+            {
+                resultado = "While";
+                Num2++;
+            }
+
+
+            //do while
+
+            do
+            {
+                MostrarMsj("Do");
+
+            } while ( Num2 > 10);
+
+
+            // for 
+
+            for (int i = 0; i < Num2; i++)
+            {              
+
+            }
+
+
+            ///
+
+
+            MostrarMsj(resultado);
+            limpiar();
+
+           
+        }
+
+
+        private void limpiar()
+        {
+            txtNombre.Text = "";
+
+        }
+
+        private void MostrarMsj(string valor)
+        {
+            MessageBox.Show(valor);
+        }
+
+
+        //private int suma(int Num1 , int Num2)
+        //{
+        //    return Num1 + Num2;
+        //}
+
+
+        //private int suma(int Num1, int Num2) => Num1 + Num2;
+
+        private int suma(int Num1, int Num2, int Num3 = 0 )
+        {
+            return Num1 + Num2 + Num3;
+        }
+
+        private double suma(string Num1, string Num2)
+        {
+
+            return Double.Parse( Num1) + Double.Parse(Num2);
+        }
+
+
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+
+            /// Array
+            int[] Numeros;
+            Numeros = new int[5] {1,2,3,4,5};
+
+            Numeros[0] = 1;
+            Numeros[1] = 2;
+
+            for (int i = 0; i < Numeros.Length -1; i++)
+            {
+                Numeros[i] = 1;
+            }
+            Array.Resize(ref Numeros, 10);
+
+            // Matrices
+
+            int[,] numeros2;
+            numeros2 = new int[2, 2] { { 1, 2 }, { 3, 4 } };
+
+
+            
+            // Split 
+
+            string nombre = txtNombre.Text;
+            string[] vtsplit = nombre.Split(',');
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            List<string> meses = new List<string>();
+
+            meses.Add("Enero");
+            meses.Add("Marzo");
+            meses.Add("Junio");
+            meses.Add("Agosto");
+
+            meses.RemoveAt(0);
+            meses.Remove("Marzo");
+            meses.Clear();
+            //string item = meses[0];
+
+            //meses.Sort();
+            //for (int i = 0; i < meses.Count; i++)
+            //{
+
+            //}
+
+            foreach (var item in meses)
+            {
+                var  detalle = item.ToString();
+
+            }
+
+            Dictionary<int, string> User = new Dictionary<int, string>();
+
+            User.Add(100,"Carlos");
+            User.Add(200, "Juan");
+            User.Add(300, "Ana");
+
+            User.Remove(100);
+            User.Clear();
+
+            //User[100] = "David";
+
+            foreach (var item in User)
+            {
+                string detalle = item.Value;
+            }
+
+            HashSet<int> conj = new HashSet<int>();
+            conj.Add(100);
+            conj.Add(200);
+            conj.Add(100);
+
+
+            DataTable dt_User = new DataTable();
+            dt_User.Columns.Add("Cedula");
+            dt_User.Columns.Add("Nombre");
+            dt_User.Columns.Add("Ciudad");
+
+            dt_User.Rows.Add();
+            dt_User.Rows.Add(new object[] { "1093", "Juan", "Pereira" });
+
+            DataRow dr_usr = dt_User.NewRow();
+            dr_usr["Cedula"] = "1088";
+            dr_usr["Nombre"] = "Carlos";
+            dr_usr["Ciudad"] = "Cartago";
+            dt_User.Rows.Add(dr_usr);
+
+            string dato;
+
+            foreach(DataRow dr in dt_User.Rows)
+            {
+                dato= dr["Nombre"].ToString();
+            }
+
+            dt_User.Rows[0]["Nombre"] = "David";
+
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                int Num;
+                Num = int.Parse(txtNombre.Text);
+
+                if (Num == 6)
+                {
+                    throw new FormatException();
+                }
+
+
+            }
+            catch (FormatException Ex)
+            {
+                MessageBox.Show("Error en formato");
+               
+            }
+            catch (OverflowException Ex)
+            {
+                MessageBox.Show("Error en Operación Art");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error en sistema intente de nuevo ");
+
+            }
+            finally
+            {
+                txtNombre.Text = "";
+          
+
+            }
+
+
+        }
+    }
+}
