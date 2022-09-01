529c7eee964b9a68c326de040229416752866b14
JUAN DAVID........ LOPEZ <juanda-1712@hotmail.com>
2022-09-01 16:47:43 -0500

    prueba

---
+++ Unidad1/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unidad1
+{
+    internal class Class1
+    {
+
+        string ada;
+    }
+}
